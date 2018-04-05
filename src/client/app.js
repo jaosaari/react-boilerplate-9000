91d1910121eb8fa3b048f44b8587fe5378531ca2
Janne Saari <jaosaari@gmail.com>
2018-04-05 11:48:39 +0200

    react & react-dom added

---
+++ src/client/app.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react'
+export default class App extends Component {
+    render() {
+        return <div>Welcome to the React Boilerplate 9000 application, state of the art in React Boilerplate applications!!!</div>
+    }
+}
